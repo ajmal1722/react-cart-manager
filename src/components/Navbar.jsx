083a0de1d63663ce6e54744c0210001444700c7f
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-02 14:27:28 +0530

    static navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import { FaShoppingBag } from 'react-icons/fa';
+import { useState } from 'react';
 
 const Navbar = () => {
+    
     return (
-        <nav className='text-white bg-red-700 max-w-screen-2xl mx-auto py-5'>
-            <div className="mx-auto max-w-screen-xl bg-blue-400">
-                <div className="d-flex">
-                    <a href="" className="text-2xl mx-3">
-                        React Cart
-                    </a>
-                    <div className="">
+        <nav className='text-white bg-red-700 max-w-screen-2xl mx-auto'>
+            <div className="mx-auto max-w-screen-xl">
+                <div className="flex justify-between">
+                    <div className="py-4">
+                        <a href="" className="text-2xl mx-3">
+                            React Cart
+                        </a>
+                    </div>
+                    <div className=" py-5">
                         <a href="" className="text-lg mx-5">Home</a>
-                        <a href="" className="text-lg">Products</a>
+                        <a href="" className="text-lg mx-5">Products</a>
+                        <a href="" className="text-lg">Contact</a>
+                    </div>
+                    <div className=" mx-4">
+                        <a href="" className="text-lg mx-5"><FaShoppingBag /></a>
                     </div>
                 </div>
             </div>
