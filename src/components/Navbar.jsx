6081c05e8de2b5bc93546efeec67f93e72ee7b2c
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-02 17:16:27 +0530

    initial side bar setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,36 @@
-import { NavLink } from 'react-router-dom';
-import { FaShoppingBag } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { IoMenuSharp, IoCart, IoClose } from 'react-icons/io5';
 import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [sidebarOpen, setSidebar] = useState(false);
     
     return (
-        <nav className='text-white bg-red-700 max-w-screen-2xl mx-auto'>
-            <div className="mx-auto max-w-screen-xl">
-                <div className="flex justify-between">
-                    <div className="py-4">
-                        <a href="" className="text-2xl mx-3">
-                            React Cart
-                        </a>
-                    </div>
-                    <div className=" py-5">
-                        <a href="" className="text-lg mx-5">Home</a>
-                        <a href="" className="text-lg mx-5">Products</a>
-                        <a href="" className="text-lg">Contact</a>
-                    </div>
-                    <div className=" mx-4">
-                        <a href="" className="text-lg mx-5"><FaShoppingBag /></a>
-                    </div>
-                </div>
+        <nav className='bg-red-700 flex justify-between px-8 py-6 text-white'>
+            <section className='flex item-center gap-4'>
+                {/* Menu Icon */}
+                <IoMenuSharp className='text-3xl'></IoMenuSharp>
+                {/* Logo */}
+                <a href="/" className='text-2xl font-bold'>
+                    React Cart
+                </a>
+            </section>
+
+            {/* Sidebar mobile menu */}
+            <div className='fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0'>
+                <section className="text-black bg-white flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 flex w-56">
+                    <IoClose className='text-3xl cursor-pointer mb-5'/>
+                    <a href="">Home</a>
+                    <a href="">Products</a>
+                    <a href="">Contact</a>
+                </section>
             </div>
+
+            <section className="">
+                {/* Cart Icon */}
+                <IoCart className='text-2xl'></IoCart>
+            </section>
         </nav>
     )
 }
