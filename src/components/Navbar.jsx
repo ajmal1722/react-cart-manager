3afcfa7f45bce9b67f8f5d64b7d0cd6f04eeac4a
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-02 18:12:10 +0530

    sidebar mobile menu is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,18 @@ import { useState } from 'react';
 const Navbar = () => {
 
     const [sidebarOpen, setSidebar] = useState(false);
+
+    const toggleNavbar = () => {
+        setSidebar(prevState => !prevState)
+    };
     
     return (
         <nav className='bg-red-700 flex justify-between px-8 py-6 text-white'>
             <section className='flex item-center gap-4'>
                 {/* Menu Icon */}
-                <IoMenuSharp className='text-3xl'></IoMenuSharp>
+                <IoMenuSharp className='text-3xl' onClick={toggleNavbar}>
+
+                </IoMenuSharp>
                 {/* Logo */}
                 <a href="/" className='text-2xl font-bold'>
                     React Cart
@@ -18,14 +24,16 @@ const Navbar = () => {
             </section>
 
             {/* Sidebar mobile menu */}
-            <div className='fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0'>
-                <section className="text-black bg-white flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 flex w-56">
-                    <IoClose className='text-3xl cursor-pointer mb-5'/>
-                    <a href="">Home</a>
-                    <a href="">Products</a>
-                    <a href="">Contact</a>
-                </section>
-            </div>
+            {sidebarOpen && (
+                <div className='fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0 '>
+                    <section className="text-black bg-white flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 flex w-56">
+                        <IoClose className='text-3xl cursor-pointer mb-5 ' onClick={toggleNavbar}/>
+                        <a href="">Home</a>
+                        <a href="">Products</a>
+                        <a href="">Contact</a>
+                    </section>
+                </div>
+            )}
 
             <section className="">
                 {/* Cart Icon */}
